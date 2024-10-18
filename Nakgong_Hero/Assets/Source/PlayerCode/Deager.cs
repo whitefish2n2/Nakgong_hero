3273d9152f65865c9877321bd41af8194af32efd
whitefish2n2 <whitefish_@naver.com>
2024-10-18 20:10:13 +0900

    Player Texture Fix
    Daihen

--- Nakgong_Hero/Assets/Source/PlayerCode/Deager.cs
+++ Nakgong_Hero/Assets/Source/PlayerCode/Deager.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Source.MonsterCode;
 using Unity.Mathematics;
 using UnityEngine;
 
@@ -12,6 +13,7 @@ public class Deager : MonoBehaviour
     [SerializeField] private Rigidbody2D rigid;
     public static bool isCrashWithWall = false;
     private bool _isThrowing;
+    private Vector3 _throwPosTemp;
     public void ThrowAt_withThrowRange(Vector3 ThrowPos, float Range)
     {
         StartCoroutine(ThrowAt(ThrowPos, Range));
@@ -29,20 +31,20 @@ public class Deager : MonoBehaviour
         
     }
 
-    private IEnumerator ThrowAt(Vector3 ThrowPos_IE, float Range_IE)
+    private IEnumerator ThrowAt(Vector3 throwPosIE, float rangeIE)
     {
-        float elapsedTime = 0f;
-        Vector3 playerPos = PlayerController.PlayerPos;
-        Vector2 dirvec = ThrowPos_IE - playerPos;
-        float MouseRange = Vector2.Distance(ThrowPos_IE, playerPos);
+        gameObject.transform.parent = null;
+        _throwPosTemp = throwPosIE;
+        Vector3 playerPos = PlayerController.Instance.playerPos;
+        Vector2 dirvec = throwPosIE - playerPos;
+        float mouseRange = Vector2.Distance(throwPosIE, playerPos);
         _isThrowing = true;
         gameObject.transform.up = dirvec.normalized;
-        while (Vector2.Distance(transform.position,playerPos) < Range_IE && Vector2.Distance(transform.position,playerPos) < MouseRange)
+        while (Vector2.Distance(transform.position,playerPos) < rangeIE && Vector2.Distance(transform.position,playerPos) < mouseRange)
         {
-            if (!isCrashWithWall && PlayerController.IsThrowing && !PlayerController.IsGetHooking)
+            if (!isCrashWithWall && PlayerController.Instance.isThrowing && !PlayerController.Instance.isGetHooking)
             {
                 rigid.velocity = dirvec * ThrowingSpeed;
-                elapsedTime += Time.deltaTime;
                 yield return null;
             }
             else
@@ -60,33 +62,49 @@ public class Deager : MonoBehaviour
         isCrashWithWall = false;
         yield return new WaitForSeconds(AirWaitTime);
         float turnbackTemp = TurnBackSpeed;
-        Vector2 dirvec = PlayerController.PlayerPos - transform.position;
-        while (Vector2.Distance(transform.position, PlayerController.PlayerPos) > 0.9f)
+        Vector2 dirvec = PlayerController.Instance.playerPos - transform.position;
+        while (Vector2.Distance(transform.position, PlayerController.Instance.playerPos) > 0.9f)
         {
-            dirvec = PlayerController.PlayerPos - transform.position;
-            transform.position = Vector2.Lerp(transform.position, PlayerController.PlayerPos, TurnBackSpeed * Time.deltaTime);
+            dirvec = _throwPosTemp - transform.position;
+            transform.position = Vector2.Lerp(transform.position, PlayerController.Instance.playerPos, TurnBackSpeed * Time.deltaTime);
             transform.up = dirvec;
             TurnBackSpeed += 3f * Time.deltaTime;
             yield return null;
         }
         TurnBackSpeed = turnbackTemp;
-        PlayerController.IsGetHooking = false;
-        PlayerController.IsThrowing = false;
+        PlayerController.Instance.isGetHooking = false;
+        PlayerController.Instance.isThrowing = false;
         GroundDeagerCheck.dontCheck = false;
-        gameObject.GetComponent<SpriteRenderer>().color = Color.clear;
+        gameObject.transform.parent = PlayerController.Instance.gameObject.transform;
+        gameObject.transform.position =
+            new Vector3(PlayerController.Instance.playerPos.x,
+                PlayerController.Instance.playerPos.y + 0.26f, 0f);
         _isThrowing = false;
         yield break;
     }
 
+    public void InstanceTurnBack()
+    {
+        GroundDeagerCheck.dontCheck = true;
+        isCrashWithWall = false;
+        PlayerController.Instance.isGetHooking = false;
+        PlayerController.Instance.isThrowing = false;
+        GroundDeagerCheck.dontCheck = false;
+        gameObject.transform.parent = PlayerController.Instance.gameObject.transform;
+        gameObject.transform.position =
+            new Vector3(PlayerController.Instance.playerPos.x,
+                PlayerController.Instance.playerPos.y + 0.26f, 0f);
+        _isThrowing = false;
+    }
+
     private void Update()
     {
-        if (!PlayerController.IsThrowing)
+        if (!PlayerController.Instance.isThrowing)
         {
-            float zRotPlus = (PlayerController.PlayerRotate.y <= 0.5f) ? -45f : 45f;
-            gameObject.transform.position =
-                new Vector3(PlayerController.PlayerPos.x,
-                PlayerController.PlayerPos.y + 0.26f, 0f);
-            gameObject.transform.rotation = Quaternion.Euler(0f,0f,zRotPlus);
+            float zRotPlus = (PlayerController.Instance.playerRotate.y <= 0.5f) ? -1f : 1f;
+            gameObject.transform.localPosition = new Vector3(0,0.26f, 0f);
+            gameObject.transform.rotation = Quaternion.Euler(0f, 0f, zRotPlus*45);
+            gameObject.transform.localScale = new Vector3(-zRotPlus, 1f, 1f);
         }
     }
 
